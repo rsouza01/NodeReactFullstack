8382bc8838e4e2717e2b35e5a8378a063ab227e3
Rodrigo Souza <rsouza01@gmail.com>
2018-10-25 00:20:11 -0300

    Front + React components

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+import Header from './Header'
+
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
     return (
         <div>
-            Hi there!!
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route path="/"             component={Landing} exact />
+                    <Route path="/surveys"      component={Dashboard}  exact />
+                    <Route path="/surveys/new"  component={SurveyNew} />
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
